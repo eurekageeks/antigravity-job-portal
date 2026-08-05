0e9cdfc1df5e078de4d5d43855dcd542073967a7
Kanishk Malhotra <malhotrakanishk59@gmail.com>
2026-08-05 17:15:41 +0530

    third commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://backend:5000',
         changeOrigin: true,
         secure: false,
       },
       '/uploads': {
-        target: 'http://localhost:5000',
+        target: 'http://backend:5000',
         changeOrigin: true,
         secure: false,
       }
